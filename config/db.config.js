cfc7e821c5ec98d5830ba71469f7ad308a4631d0
umaaamm <umam.tekno@gmail.com>
2023-05-24 00:27:49 +0700

    add config db

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,23 @@
-module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "postgres",
-    DB: "kitawarga",
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PASSWORD: "postgres",
+//     DB: "kitawarga",
+//     dialect: "postgres",
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     }
+//   };
+
+
+  module.exports = {
+    HOST: process.env.INSTANCE_UNIX_SOCKET,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASS,
+    DB: process.env.DB_NAME,
     dialect: "postgres",
     pool: {
       max: 5,
