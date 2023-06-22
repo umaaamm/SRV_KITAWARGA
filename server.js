f36ae3b20971e73e1f2fb0bcd51088d0ad2287d6
umaaamm <umam.tekno@gmail.com>
2023-06-22 17:15:42 +0700

    [main][cors] : add cors domain

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:3000"
+  origin: "http://api.kitawarga.com:3000"
 };
 
 app.use(cors(corsOptions));
