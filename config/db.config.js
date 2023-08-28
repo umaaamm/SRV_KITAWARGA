c47b3f2557b47370c2a29b39d8d2075fa08e5ec0
Kurniawan Gigih Lutfi Umam <umam.tekno@gmail.com>
2023-08-28 16:25:32 +0700

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,21 @@
+// module.exports = {
+//     HOST: "34.31.73.252",
+//     USER: "postgres",
+//     PASSWORD: "postgres",
+//     DB: "kitawarga",
+//     dialect: "postgres",
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     }
+//   };
+
 module.exports = {
-    HOST: "34.31.73.252",
-    USER: "postgres",
-    PASSWORD: "postgres",
+    HOST: "db-postgresql-sgp1-57048-do-user-14579204-0.b.db.ondigitalocean.com",
+    USER: "doadmin",
+    PASSWORD: "AVNS_N-dYrq_naXbjoya1FjE",
     DB: "kitawarga",
     dialect: "postgres",
     pool: {
