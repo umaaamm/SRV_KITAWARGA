0ce5602c0d0c8b02a27166b65025f68c087b54c3
Kurniawan Gigih Lutfi Umam <umam.tekno@gmail.com>
2023-09-06 00:14:00 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
