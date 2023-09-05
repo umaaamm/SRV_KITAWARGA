21e12880be8ae4e7fd5fafe1322d9955224b56cf
Kurniawan Gigih Lutfi Umam <umam.tekno@gmail.com>
2023-09-05 23:44:05 +0700

    Update server.js

--- server.js
+++ server.js
@@ -174,7 +174,7 @@ require('./routes/user.routes')(app);
 require('./routes/admin.routes')(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
