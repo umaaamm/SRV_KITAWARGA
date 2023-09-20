91341f150820ac69f004b40e3d35f44062367c91
umaaamm <umam.tekno@gmail.com>
2023-09-20 21:22:19 +0700

    [main][pemasukan] : pemasukan

--- controllers/pemasukan.controller.js
+++ controllers/pemasukan.controller.js
@@ -4,37 +4,39 @@ const Perumahan = db.perumahan;
 const Warga = db.daftarWarga;
 
 exports.addPemasukan = (req, res) => {
-    Warga.findOne({
-        where: {
-            biaya_ipl: req.body.data.amount,
-            id_perumahan: '4872a5d0-3428-11ee-be56-0242ac120002'
-        }
-    }).then((warga) => {
-        Pemasukan.create({
-            id_transaksi: req.body.data.id,
-            id_warga: warga.id_warga,
-            nama_pembayar: warga.nama_warga,
-            nomor_rumah: warga.nomor_rumah,
-            tanggal_transaksi: Math.floor(new Date().getTime() / 1000),
-            nilai_transaksi: req.body.data.amount,
-        })
-            .then(async (user) => {
-                const PerumahanData = await Perumahan.findOne({
-                    where: {
-                        id_perumahan: '4872a5d0-3428-11ee-be56-0242ac120002'
-                    }
-                });
 
-                await Perumahan.update({
-                    saldo_perumahan: parseInt(PerumahanData.saldo_perumahan) + (parseInt(req.body.data.amount)),
-                }, { where: { id_perumahan: '4872a5d0-3428-11ee-be56-0242ac120002' } });
+    console.log('mlmldmgldmg', req.body);
+    // Warga.findOne({
+    //     where: {
+    //         biaya_ipl: req.body.data.amount,
+    //         id_perumahan: '4872a5d0-3428-11ee-be56-0242ac120002'
+    //     }
+    // }).then((warga) => {
+    //     Pemasukan.create({
+    //         id_transaksi: req.body.data.id,
+    //         id_warga: warga.id_warga,
+    //         nama_pembayar: warga.nama_warga,
+    //         nomor_rumah: warga.nomor_rumah,
+    //         tanggal_transaksi: Math.floor(new Date().getTime() / 1000),
+    //         nilai_transaksi: req.body.data.amount,
+    //     })
+    //         .then(async (user) => {
+    //             const PerumahanData = await Perumahan.findOne({
+    //                 where: {
+    //                     id_perumahan: '4872a5d0-3428-11ee-be56-0242ac120002'
+    //                 }
+    //             });
 
-                res.status(200).send({ message: "Pemasukan berhasil ditambah!." });
-            })
-            .catch(err => {
-                res.status(500).send({ message: err.message });
-            });
-    })
+    //             await Perumahan.update({
+    //                 saldo_perumahan: parseInt(PerumahanData.saldo_perumahan) + (parseInt(req.body.data.amount)),
+    //             }, { where: { id_perumahan: '4872a5d0-3428-11ee-be56-0242ac120002' } });
+
+    //             res.status(200).send({ message: "Pemasukan berhasil ditambah!." });
+    //         })
+    //         .catch(err => {
+    //             res.status(500).send({ message: err.message });
+    //         });
+    // })
 };
 
 
