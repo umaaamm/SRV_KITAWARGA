e5fe26fd3e2e1a204bd14cb637d265355da6e243
“umaaamm” <umam.tekno@gmail.com>
2023-10-09 16:32:36 +0700

    add platform fee

--- services/axios.js
+++ services/axios.js
@@ -8,6 +8,7 @@ axiosInstance.interceptors.request.use(
     async (config) => {
         config.headers['Authorization'] = 'Basic eG5kX3Byb2R1Y3Rpb25fM1FDbTVZZW5oamI5VHBvcDZZUm01aGFiN3RhZWVhT3N1RTE4U3d2ZE82STg3WXdFNVNFSHJwTlY5dGtRYXJOOktsb3AxMjM0';
         config.headers['api-version'] = '2022-07-31';
+        config.headers['with-fee-rule'] = 'xpfeeru_174c015b-2151-499c-88d6-d676190c7a1a'
 
         return config;
     },
